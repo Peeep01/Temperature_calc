76945f697cec139b4b25e0c7af7983e7c352af27
Peeep01 <agrosev18@gmail.com>
2022-07-28 02:56:54 +0300

    Добавьте файлы проекта.

---
+++ Practic/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Temperature_calculator
+{
+    internal class Program
+    {
+       public static void Main(string[] args)
+       {
+            while (true)
+            {
+                Console.Write("Введите число по цельсию: ");
+                int celsius = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Кельвин = {0}", celsius + 273);
+                Console.WriteLine("Фаренгейт = {0}", celsius * 18 / 10 + 32);
+                
+            }
+            
+       }
+    }
+}
